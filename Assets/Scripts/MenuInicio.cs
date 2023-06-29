4a4a78b3949c6dd322e27882a031a41319d4e499
yaela <yael.anaya@uabc.edu.mx>
2023-06-29 06:43:09 -0700

    arreglitos en un fallo del cursor en el menú principal.

--- Assets/Scripts/MenuInicio.cs
+++ Assets/Scripts/MenuInicio.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicio : MonoBehaviour
 {
+    private void Update()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void Comenzar()
     {
         SceneManager.LoadScene(1);
